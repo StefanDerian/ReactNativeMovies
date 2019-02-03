9572315336efc313c2e91500ec18bc00ccd31aa1
StefanDerian2 <stefan.derian@gmail.com>
2019-02-03 21:10:33 +1100

    finished adding the native base after a long errors

--- App.js
+++ App.js
@@ -1,15 +1,72 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet } from 'react-native';
+import { Container, Body, Title, Header, Content, List, ListItem, Text , Left, Right, Button, Icon} from 'native-base';
+
+
+
+
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
-    );
+
+
+  constructor(props) {
+    super(props);
+    this.state = { loading: true };
   }
+
+  async componentWillMount() {
+    await Expo.Font.loadAsync({
+      'Roboto': require('native-base/Fonts/Roboto.ttf'),
+      'Roboto_medium': require('native-base/Fonts/Roboto_medium.ttf'),
+    });
+    this.setState({ loading: false });
+  }
+  render() {
+    if (this.state.loading) {
+      return <Expo.AppLoading />;
+    }
+    return (
+
+        <Container>
+          <Header>
+            <Left>
+              <Button transparent>
+                <Icon name='menu' />
+              </Button>
+            </Left>
+            <Body>
+              <Title>Header</Title>
+            </Body>
+            <Right />
+          </Header>
+          <Content>
+            <List>
+              <ListItem itemDivider>
+                <Text>A</Text>
+              </ListItem>
+              <ListItem>
+                <Text>Aaron Bennet</Text>
+              </ListItem>
+              <ListItem>
+                <Text>Ali Connors</Text>
+              </ListItem>
+              <ListItem itemDivider>
+                <Text>B</Text>
+              </ListItem>
+              <ListItem>
+                <Text>Bradley Horowitz</Text>
+              </ListItem>
+            </List>
+          </Content>
+
+        </Container>
+
+    );
+
+
+
+  }
+
 }
 
 const styles = StyleSheet.create({
---
+++ Component/MoviesContainer.js
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import { StyleSheet } from 'react-native';
+import { Container, Body, Title, Header, Content, List, ListItem, Text , Left, Right, Button, Icon} from 'native-base';
+
+
+export default class App extends React.Component {
+  
+
+}
